02778bf75dd35314bfc5a13d92269f35cbc33631
rittikbasu <irittik@gmail.com>
2024-08-13 16:25:37 +0530

    feat: reduce z-index for backdrop blur to take effect on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const { cartItems } = useContext(CartContext);
 
   return (
-    <header className="fixed top-0 w-full z-50 mt-4 px-2 sm:px-0">
+    <header className="fixed top-0 w-full z-30 mt-4 px-2 sm:px-0">
       <div className="flex justify-between items-center max-w-7xl py-2 px-8 sm:py-4 sm:px-16 bg-zinc-100/80 border webkit-backdrop-blur rounded-full">
         <h1 className="sm:text-3xl text-zinc-700 text-2xl font-bold font-mono active:scale-50 transition duration-300">
           <Link href="/">EzBuy</Link>
