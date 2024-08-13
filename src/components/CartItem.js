5b690971e0c8166ce510b8612e27747461e7c8b7
rittikbasu <irittik@gmail.com>
2024-08-13 06:26:23 +0530

    feat: add price breakdown + coupon + animation for empty cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,41 +4,60 @@ import { CartContext } from "../context/CartContext";
 
 const CartItem = ({ item }) => {
   const { removeFromCart, updateQuantity } = useContext(CartContext);
+  const discountedPrice = item.price - item.discount;
 
   return (
     <div className="flex items-center justify-between border-b py-4">
-      <Image
-        src={item.image}
-        alt={item.title}
-        width={64}
-        height={64}
-        className="h-16 w-16 object-contain"
-      />
-      <h2 className="flex-1 ml-4">{item.title}</h2>
-      <div className="flex items-center">
-        <button
-          className="px-2"
-          onClick={() => updateQuantity(item.id, item.quantity - 1)}
-        >
-          -
-        </button>
-        <span className="px-2">{item.quantity}</span>
-        <button
-          className="px-2"
-          onClick={() => updateQuantity(item.id, item.quantity + 1)}
-        >
-          +
-        </button>
+      <div className="flex flex-col items-center">
+        <Image
+          src={item.image}
+          alt={item.title}
+          width={100}
+          height={100}
+          className="h-24 w-24 object-contain"
+        />
+        <div className="flex items-center space-x-2 border rounded-lg mt-2">
+          <button
+            className="w-8 px-2"
+            onClick={() => updateQuantity(item.id, item.quantity - 1)}
+          >
+            -
+          </button>
+          <span className="px-2 border-l border-r w-8 text-center">
+            {item.quantity}
+          </span>
+          <button
+            className="w-8 px-2"
+            onClick={() => updateQuantity(item.id, item.quantity + 1)}
+          >
+            +
+          </button>
+        </div>
       </div>
-      <p className="w-24 text-right">
-        ${(item.price * item.quantity).toFixed(2)}
-      </p>
-      <button
-        className="ml-4 text-red-600"
-        onClick={() => removeFromCart(item.id)}
-      >
-        Remove
-      </button>
+      <div className="flex flex-col ml-4 flex-1">
+        <h2 className="text-left line-clamp-3 sm:line-clamp-2">{item.title}</h2>
+        <p className="text-left text-sm capitalize text-gray-500">
+          {item.brand}
+        </p>
+        <div className="flex items-center space-x-4 mt-3">
+          <div className="flex items-center space-x-2">
+            <p className="text-right">
+              ${(discountedPrice * item.quantity).toFixed(2)}
+            </p>
+            {item.discount > 0 && (
+              <span className="line-through text-gray-500">
+                ${(item.price * item.quantity).toFixed(2)}
+              </span>
+            )}
+          </div>
+          <button
+            className="text-red-600"
+            onClick={() => removeFromCart(item.id)}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,27 +1,136 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Link from "next/link";
 import { CartContext } from "@/context/CartContext";
 import CartItem from "@/components/CartItem";
 
 const CartPage = () => {
   const { cartItems } = useContext(CartContext);
+  const [coupon, setCoupon] = useState("");
+  const [isCouponValid, setIsCouponValid] = useState(false);
+  const [couponError, setCouponError] = useState("");
 
-  const calculateSubtotal = () =>
+  const handleCouponCheck = (coupon) => {
+    if (coupon === "profilefyi") {
+      setCoupon("");
+      setIsCouponValid(true);
+      setCouponError("");
+    } else {
+      setIsCouponValid(false);
+      setCouponError("Invalid coupon code.");
+    }
+  };
+
+  const calculateSubtotal = () => {
+    const subtotal = cartItems.reduce((sum, item) => {
+      const discount = item.discount || 0;
+      const discountedPrice = item.price - discount;
+      return sum + discountedPrice * item.quantity;
+    }, 0);
+
+    if (coupon.toLowerCase() === "profilefyi") {
+      return subtotal * 0.9;
+    }
+
+    return subtotal;
+  };
+
+  const calculateOriginalPrice = () =>
     cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
+  const calculateDiscount = () =>
+    cartItems.reduce(
+      (sum, item) => sum + (item.discount || 0) * item.quantity,
+      0
+    );
+
   return (
-    <div className="container mx-auto p-4 mt-28">
-      <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+    <div className="mx-auto p-4 mt-20 sm:mt-28">
+      <Link href="/" className="text-blue-500 hover:underline mb-4 block">
+        &larr; Back to Products
+      </Link>
+      <h1 className="text-3xl text-zinc-700 font-bold mb-6">Your Cart</h1>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <div className="flex justify-center items-center min-h-[60vh]">
+          <video
+            src="/empty_cart.webm"
+            autoPlay
+            loop
+            className="w-full max-w-md"
+          />
+        </div>
       ) : (
         <>
           {cartItems.map((item) => (
             <CartItem key={item.id} item={item} />
           ))}
-          <div className="text-right mt-6">
-            <h2 className="text-2xl font-bold">
-              Subtotal: ${calculateSubtotal().toFixed(2)}
+
+          <div className="bg-white mt-6">
+            <label
+              className="block text-lg font-semibold mb-2"
+              htmlFor="coupon"
+            >
+              Apply Coupon
+            </label>
+            <div className="flex mb-4 sm:max-w-72 w-full border border-gray-300 rounded-lg overflow-hidden">
+              <input
+                type="text"
+                id="coupon"
+                value={coupon}
+                onChange={(e) => setCoupon(e.target.value.toUpperCase())}
+                className="flex-grow p-2 focus:outline-none"
+                placeholder="Enter coupon code"
+              />
+              <button
+                className={`p-2 bg-lime-400 text-white ${
+                  isCouponValid
+                    ? "hover:bg-lime-500"
+                    : "opacity-50 cursor-not-allowed"
+                }`}
+                disabled={!isCouponValid}
+                onClick={handleCouponCheck}
+              >
+                Apply
+              </button>
+              {couponError && (
+                <p className="text-red-500 mt-2">{couponError}</p>
+              )}
+            </div>
+            <h2 className="text-lg font-semibold mb-4">
+              Price Details ({cartItems.length} Items)
             </h2>
+            <div className="flex justify-between mb-2">
+              <span className="">Total MRP</span>
+              <span className="">${calculateOriginalPrice().toFixed(2)}</span>
+            </div>
+            <div className="flex justify-between mb-2">
+              <span className="">Discount on MRP</span>
+              <span className=" text-green-600">
+                - ${calculateDiscount().toFixed(2)}
+              </span>
+            </div>
+            {isCouponValid && (
+              <div className="flex justify-between mb-2">
+                <span className="">Coupon Discount</span>
+                <span className="text-green-600">
+                  {coupon.toLowerCase() === "profilefyi" ? "- 10%" : "- $0.00"}
+                </span>
+              </div>
+            )}
+            <div className="flex justify-between mb-2">
+              <span className="">Platform Fee</span>
+              <span className="">$2.00</span>
+            </div>
+            <div className="flex justify-between mb-2">
+              <span className="">Shipping Fee</span>
+              <span className=" text-green-600">FREE</span>
+            </div>
+            <hr className="my-2" />
+            <div className="flex justify-between mt-2">
+              <span className="text-xl font-bold">Total Amount</span>
+              <span className="text-xl font-bold">
+                ${(calculateSubtotal() + 20).toFixed(2)}
+              </span>
+            </div>
           </div>
         </>
       )}
