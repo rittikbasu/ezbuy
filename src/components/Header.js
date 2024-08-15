3545edce014e5fbbcfc134b19c38227d91f99857
rittikbasu <irittik@gmail.com>
2024-08-15 07:50:52 +0530

    feat: make header width responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
   const { cartItems } = useContext(CartContext);
 
   return (
-    <header className="fixed top-0 w-full z-30 mt-4 px-2 sm:px-0">
-      <div className="flex justify-between items-center max-w-7xl py-2 px-8 sm:py-4 sm:px-16 bg-zinc-100/80 border webkit-backdrop-blur rounded-full">
+    <header className="fixed top-0 w-full z-30 mt-4 px-2 xl:px-0">
+      <div className="flex justify-between items-center xl:max-w-7xl lg:max-w-5xl md:max-w-3xl py-2 px-8 sm:py-4 sm:px-16 bg-zinc-100/80 border webkit-backdrop-blur rounded-full mx-auto xl:mx-0">
         <h1 className="sm:text-3xl text-zinc-700 text-2xl font-bold font-mono active:scale-50 transition duration-300">
           <Link href="/">EzBuy</Link>
         </h1>
